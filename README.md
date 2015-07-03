d7c6a1e97ddc95f93a1f4436293c25de8ea00c64
Maruton Maru <marutongit@gmail.com>
2015-07-03 11:44:17 +0900

    First commit. Project type Unity4.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sample_AramtureDoor
+
+## What is this?
+
+Sample project for Unity5.    
+Armature animation mesh with blender.    
+
+## Feature
+
+Control door Open/Close.    
+Use to armature animation that make in blender, and fbx export.    
+Import those to Unity, and drive in script(C#).    
+
+
+## How to use
+
+See blog: アニメーション付メッシュのBlenderでExport, UnityでImport    
+http://dennou-note.blogspot.jp/2014/06/unity-exportimport-blender.html  
+
+
+## License
+
+English, Please see 'LICENSE' file.  
+Japanese, Please see 'LICENSE_JP' file.  
+
---
+++ Sample_AramtureDoor/Assets/Script/DoorTest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTest : MonoBehaviour {
+	void Awake(){}
+	void Start (){}
+	void OnApplicationFocus(){}
+	void OnApplicationQuit(){}
+	void OnApplicationPause(bool pauseStatus) {
+		if(pauseStatus){ //Suspend (back to OS)
+		}
+		else{ //Resume (From OS)
+		}
+	}
+	void OnGUI(){
+		DrawGUI(); // 操作ボタン、2D文字描画
+	}
+	void Update() {
+		if(Application.platform == RuntimePlatform.Android && Input.GetKey(KeyCode.Escape)){//戻るボタン対応
+			Application.Quit();
+		}
+	}
+	//============================================================
+	void DrawGUI(){
+		//if(true)return;
+		//Debug.Log ("OnGUI:");
+		GUIStyle labelStyle;	//!< GUIフォント表示用スタイル
+		int fontRetio = 18;
+		
+		labelStyle = new GUIStyle();
+		labelStyle.fontSize = Screen.width / fontRetio; // Font size
+		labelStyle.normal.textColor = Color.grey;
+		labelStyle.wordWrap = true;
+
+		float ScrnHor = (float)Screen.width;
+		float ScrnVert = (float)Screen.height;
+		Rect rectBtn = new Rect(ScrnHor*0.12f,ScrnVert*0.10f,ScrnHor*0.76f,ScrnVert*0.05f);
+		if(GUI.Button(rectBtn, "[OPEN]")) {
+			animation["Open"].speed = 4.0f;//再生速度指定
+			animation.Play("Open");
+		}
+		rectBtn = new Rect(ScrnHor*0.12f,ScrnVert*0.15f,ScrnHor*0.76f,ScrnVert*0.05f);
+		if(GUI.Button(rectBtn, "[CLOSE]")) {
+			animation["CLose"].speed = 4.0f;//再生速度指定
+			animation.Play("CLose");
+		}
+	}
+}
